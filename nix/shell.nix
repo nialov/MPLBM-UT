fdc1c34df485ce7403550c83ca149072c4a1fd8d
nialov <nikolasovaskainen@gmail.com>
2024-11-26 17:01:54 +0200

    refactor(shell): use appimage fhs base

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,46 +1,49 @@
-{ buildFHSUserEnv, lib, ... }:
-buildFHSUserEnv {
-  name = "MPLBM-UT-shell";
+{ buildFHSUserEnv, lib, appimageTools, ... }:
 
-  targetPkgs = fhsPkgs:
+let
 
-    let
-      pythons = builtins.map (python:
-        python.withPackages (p: [ p.setuptools p.pip p.wheel p.virtualenv ]))
-        (lib.attrValues { inherit (fhsPkgs) python3; });
-    in lib.attrValues {
-      inherit (fhsPkgs)
-        fish cacert gcc expat zlib pkg-config wget openmpi mpich cmake gnumake
-        libGL;
-      glibDev = fhsPkgs.glib.dev;
-      libffiDev = fhsPkgs.libffi.dev;
-      inherit (fhsPkgs.xorg) libX11 libXrender;
-      # cmake-2 = let pkgs = import inputs.nixpkgs-cmake-2 { inherit system; };
-      # in pkgs.cmake;
+  base = appimageTools.defaultFhsEnvArgs;
 
-    } ++ pythons;
-  runScript = ''
-    bash "$@"
-  '';
-  # let
-  #   app = writeShellApplication {
-  #     name = "entrypoint";
-  #     text = ''
-  #       exec ${fish}/bin/fish "$@"
-  #     '';
+  customConfig = {
+    name = "MPLBM-UT-shell";
 
-  #   };
-  # in "${app}/bin/entrypoint";
-  #writeScript "entrypoint.sh" ''
-  #  #!/usr/bin/env bash
+    targetPkgs = fhsPkgs:
 
-  #  exec /usr/bin/env fish "$@"
-  #'';
+      let
+        pythons = builtins.map (python:
+          python.withPackages (p: [ p.setuptools p.pip p.wheel p.virtualenv ]))
+          (lib.attrValues { inherit (fhsPkgs) python3; });
+        basePkgs = base.targetPkgs fhsPkgs;
+      in lib.attrValues {
+        inherit (fhsPkgs)
+          fish cacert gcc pkg-config wget openmpi mpich cmake gnumake libffi;
 
-  profile =
-
-    ''
-      test ! -f ".venv/bin/activate" && python -m venv .venv
-      test -f ".venv/bin/activate" && source ".venv/bin/activate"
+      } ++ pythons ++ basePkgs;
+    extraOutputsToInstall = [ "dev" ];
+    runScript = ''
+      bash "$@"
     '';
-}
+    # let
+    #   app = writeShellApplication {
+    #     name = "entrypoint";
+    #     text = ''
+    #       exec ${fish}/bin/fish "$@"
+    #     '';
+
+    #   };
+    # in "${app}/bin/entrypoint";
+    #writeScript "entrypoint.sh" ''
+    #  #!/usr/bin/env bash
+
+    #  exec /usr/bin/env fish "$@"
+    #'';
+
+    profile =
+
+      ''
+        test ! -f ".venv/bin/activate" && python -m venv .venv
+        test -f ".venv/bin/activate" && source ".venv/bin/activate"
+      '';
+  };
+
+in buildFHSUserEnv (lib.recursiveUpdate base customConfig)
